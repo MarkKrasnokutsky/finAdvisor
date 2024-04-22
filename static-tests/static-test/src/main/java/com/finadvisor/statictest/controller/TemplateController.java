6216fa4360c504f211bcdf256bdf4a359d55d2c4
Mark Krasnokuysky <Mark120320041234@mail.ru>
2024-04-23 04:20:18 +0500

    #10 Правки в вёрстке шаблона. Форматированный и структурный вывод информации о сигнале.

--- static-tests/static-test/src/main/java/com/finadvisor/statictest/controller/TemplateController.java
+++ static-tests/static-test/src/main/java/com/finadvisor/statictest/controller/TemplateController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TemplateController {
@@ -17,9 +18,16 @@ public class TemplateController {
     @GetMapping(value = {"/stock/{secid}/{limit}/{start}"})
     public String stockData(Model model, @PathVariable String secid, @PathVariable int limit, @PathVariable int start) {
         ArrayList<Stock> stockData = serviceStock.fetchStocks(secid, limit, start);
-        ArrayList<Stock> lastFractalData = serviceStock.findLastSignal(stockData);
+        ArrayList<Stock> lastSignalData = serviceStock.findLastSignal(stockData);
+        List<Stock> subListFractal = lastSignalData.subList(0, 5);
+        Stock breakdown = lastSignalData.get(5);
+        Stock signal = lastSignalData.get(6);
+        Stock longPoint = lastSignalData.get(7);
         model.addAttribute("stockData", stockData);
-        model.addAttribute("lastSignalData", lastFractalData);
+        model.addAttribute("subListFractal", subListFractal);
+        model.addAttribute("breakdown", breakdown);
+        model.addAttribute("signal", signal);
+        model.addAttribute("longPoint", longPoint);
         return "index";
     }
 }
