b3e4cf1a6bdb3acd88b69199bcf2fb2304066092
Mark Krasnokuysky <Mark120320041234@mail.ru>
2024-05-11 03:52:00 +0500

    #28 Добавление контроллера аутентификации и защищенного контроллера для авторизированных пользователей

---
+++ backend/finAdvisor/src/main/java/com/complex/finAdvisor/controller/SecurityController.java
@@ -0,0 +1,80 @@
+package com.complex.finAdvisor.controller;
+
+import com.complex.finAdvisor.config.JwtCore;
+import com.complex.finAdvisor.dto.SigninRequest;
+import com.complex.finAdvisor.dto.SignupRequest;
+import com.complex.finAdvisor.entity.User;
+import com.complex.finAdvisor.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class SecurityController {
+    private UserRepository userRepository;
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    public void setAuthenticationManager(@Qualifier("authenticationManager") AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    private JwtCore jwtCore;
+    @Autowired
+    public void setJwtCore(JwtCore jwtCore) {
+        this.jwtCore = jwtCore;
+    }
+
+    @PostMapping("/signup")
+    ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest) {
+        if (userRepository.existsByUsername(signupRequest.getUsername())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Choose different name");
+        }
+        if (userRepository.existsByEmail(signupRequest.getEmail())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Choose different email");
+        }
+        if (signupRequest.getPassword() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Password cannot be null");
+        }
+        User user = new User();
+        user.setUsername(signupRequest.getUsername());
+        user.setEmail(signupRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.ok("Added user successfully");
+    }
+
+    @PostMapping("/signin")
+    ResponseEntity<?> signin(@RequestBody SigninRequest signinRequest) {
+        Authentication authentication = null;
+        try {
+            authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signinRequest.getUsername(), signinRequest.getPassword()));
+        }
+        catch (BadCredentialsException e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtCore.generateToken(authentication);
+        return ResponseEntity.ok(jwt);
+    }
+}
