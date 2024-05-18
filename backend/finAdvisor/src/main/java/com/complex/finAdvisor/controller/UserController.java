cec2fef508e65a6dab1bbe29577803da712eb49b
Mark Krasnokuysky <Mark120320041234@mail.ru>
2024-05-18 06:56:29 +0500

    #39 Добавление эндпоинтов для изменения и получения телеграм-никнейма. Добавление dto для post-запроса на обновление никнейма.

--- backend/finAdvisor/src/main/java/com/complex/finAdvisor/controller/UserController.java
+++ backend/finAdvisor/src/main/java/com/complex/finAdvisor/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.complex.finAdvisor.controller;
 
+import com.complex.finAdvisor.dto.TelegramRequest;
 import com.complex.finAdvisor.entity.TariffEntity;
 import com.complex.finAdvisor.entity.UserEntity;
 import com.complex.finAdvisor.repository.UserRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Tag(name = "Пользователи", description = "Контроллер для работы с пользователями")
@@ -30,5 +35,28 @@ public class UserController {
     @Operation(summary = "Возвращает текущий тариф пользователя по его id")
     public TariffEntity getTariffByUserId(@PathVariable Long id) {
         return userRepository.findTariffByUserId(id);
+    }
+
+    @PostMapping("/updateTelegram")
+    @Operation(summary = "Обновляет телеграм-никнейм полььзователя в базе данных")
+    public ResponseEntity<?> updateTelegram(@RequestBody TelegramRequest telegramRequest, Principal principal) {
+        Optional<UserEntity> currentUser = userRepository.findByUsername(principal.getName());
+        currentUser.ifPresent(userEntity -> {
+            userEntity.setTgNickname(telegramRequest.getNickname());
+            userRepository.save(userEntity);
+        });
+        return ResponseEntity.ok("Updated telegram nickname to " + telegramRequest.getNickname() + "for user - " + principal.getName());
+    }
+    @GetMapping("/getTelegram")
+    @Operation(summary = "Возвращает телеграм ник текущего авторизированного пользователя")
+    public String updateTelegram(Principal principal) {
+        Optional<UserEntity> currentUser = userRepository.findByUsername(principal.getName());
+        var ref = new Object() {
+            String telegramNickname;
+        };
+        currentUser.ifPresent(userEntity -> {
+            ref.telegramNickname = userEntity.getTgNickname();
+        });
+        return ref.telegramNickname;
     }
 }
